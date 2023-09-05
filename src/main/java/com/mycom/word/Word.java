22cd49abc63caccf7ba662c2e1c41fb51b7fec6f
GoldenWo1f <816dong@handong.ac.kr>
2023-09-05 16:46:35 +0900

    Create project

---
+++ src/main/java/com/mycom/word/Word.java
@@ -0,0 +1,49 @@
+package com.mycom.word;
+
+public class Word {
+    public Word(){}
+    public Word(int id, int level, String word, String meaning) {
+        this.id = id;
+        this.level = level;
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    int id;
+    int level;
+    String word;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    String meaning;
+}
