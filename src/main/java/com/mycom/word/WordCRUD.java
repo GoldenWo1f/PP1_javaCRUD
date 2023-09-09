c57e8c2254b40f8a3754052f5cceb0c2e2f91bf0
GoldenWo1f <816dong@handong.ac.kr>
2023-09-09 18:12:04 +0900

    Java CRUD - Delete

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -74,7 +74,7 @@ public class WordCRUD implements ICRUD{
         System.out.print("=> 수정할 단어 검색 : ");
         String keyword = s.next();
         ArrayList<Integer> idlist = this.listAll(keyword);
-        System.out.print("=> 수정할 번호 검색 : ");
+        System.out.print("=> 수정할 번호 선택 : ");
         int id = s.nextInt();
         s.nextLine();
         System.out.print("뜻 입력 : ");
@@ -85,5 +85,18 @@ public class WordCRUD implements ICRUD{
     }
 
     public void deleteItem() {
+        System.out.print("=> 삭제할 단어 검색 : ");
+        String keyword = s.next();
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 삭제할 번호 선택 : ");
+        int id = s.nextInt();
+        s.nextLine();
+        System.out.print("=> 정말로 삭제하래요?(Y/N) : ");
+        String answer = s.next();
+        if(answer.equalsIgnoreCase("y")){
+            list.remove((int)idlist.get(id-1));
+            System.out.println("단어가 삭제되었습니다. ");
+        }else
+            System.out.println("취소되었습니다. ");
     }
 }
