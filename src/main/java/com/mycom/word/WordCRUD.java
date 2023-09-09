4d05c7451dad1c458c6e2bd989c2ef35daa5353a
GoldenWo1f <816dong@handong.ac.kr>
2023-09-09 17:54:32 +0900

    Java CRUD - Read

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -36,9 +36,24 @@ public class WordCRUD implements ICRUD{
             System.out.print(i+1 + " ");
             System.out.println(list.get(i).toString());
         }
-        System.out.println("------------------------------\n");
+        System.out.println("------------------------------");
     }
+    public ArrayList<Integer> listAll(String keyword){
+        ArrayList<Integer> idlist = new ArrayList<>();
+        int j = 0;
+        System.out.println("------------------------------");
+        for(int i=0; i<list.size(); i++){
+            String word = list.get(i).getWord();
+            if(!word.contains(keyword)) continue;
+            System.out.print(j+1 + " ");
+            System.out.println(list.get(i).toString());
+            idlist.add(i);
+            j++;
+        }
+        System.out.println("------------------------------");
 
+        return idlist;
+    }
 
     @Override
     public int update(Object obj) {
@@ -53,5 +68,22 @@ public class WordCRUD implements ICRUD{
     @Override
     public void selectOne(int id) {
 
+    }
+
+    public void updatItem() {
+        System.out.print("=> 수정할 단어 검색 : ");
+        String keyword = s.next();
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 수정할 번호 검색 : ");
+        int id = s.nextInt();
+        s.nextLine();
+        System.out.print("뜻 입력 : ");
+        String meaning = s.nextLine();
+        Word word = list.get(idlist.get(id-1));
+        word.setMeaning(meaning);
+        System.out.println("단어가 수정되었습니다. ");
+    }
+
+    public void deleteItem() {
     }
 }
